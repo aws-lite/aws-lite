58aaeb572c9d2670944440676507784fa30e4f9f
Ryan Block <rblock@gmail.com>
2023-10-05 18:57:52 -0700

    Just a wee bit more vendor caching shenanigans

--- src/lib.js
+++ src/lib.js
@@ -1,22 +1,28 @@
 let { stat } = require('fs/promises')
-let { marshall, unmarshall } = require('./_vendor/aws')
+let aws
 
 // AWS-flavored JSON stuff
 function marshaller (method, obj, awsjsonSetting) {
+  if (!aws) {
+    // Only require the vendor if + when it's actually needed
+    // eslint-disable-next-line
+    aws = require('./_vendor/aws')
+  }
+
   // We may not be able to AWS JSON-[en|de]code the whole payload, check for specified keys
   if (Array.isArray(awsjsonSetting)) {
     return Object.entries(obj).reduce((acc, [ k, v ]) => {
-      if (awsjsonSetting.includes(k)) acc[k] = method(v)
+      if (awsjsonSetting.includes(k)) acc[k] = aws[method](v)
       else acc[k] = v
       return acc
     }, {})
   }
   // Otherwise, just AWS JSON-[en|de]code the whole thing
-  return method(obj)
+  return aws[method](obj)
 }
 let awsjson = {
-  marshall: marshaller.bind({}, marshall),
-  unmarshall: marshaller.bind({}, unmarshall),
+  marshall: marshaller.bind({}, 'marshall'),
+  unmarshall: marshaller.bind({}, 'unmarshall'),
 }
 
 async function exists (file) {
