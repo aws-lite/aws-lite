2f334adcc4b34371b2cb242bc31ee1ee30233d76
Ryan Block <rblock@gmail.com>
2023-10-06 13:06:13 -0700

    I can tidy up that script a bit more

--- scripts/get-regions.js
+++ scripts/get-regions.js
@@ -2,22 +2,17 @@ let { join } = require('path')
 let { writeFileSync } = require('fs')
 let awsLite = require('../')
 
-// Get the list of Lambda regions
-async function getRegions (aws) {
-  let Path = '/aws/service/global-infrastructure/regions'
-  let results = await aws.ssm.GetParametersByPath({ Path, paginate: true })
-  let regions = results.Parameters
-    .map(({ Value }) => Value)
-    .sort()
-    .reverse()
-  if (!regions.length) throw Error('No regions found! Weird.')
-
-  return regions
-}
-
+// Get the list of current AWS regions
 async function main () {
   let aws = await awsLite({ region: 'us-east-1' })
-  let regions = await getRegions(aws)
+
+  let results = await aws.ssm.GetParametersByPath({
+    Path: '/aws/service/global-infrastructure/regions',
+    paginate: true
+  })
+  let regions = results.Parameters.map(({ Value }) => Value).sort().reverse()
+  if (!regions.length) throw Error('No regions found! Weird.')
+
   let file = join(__dirname, '..', 'src', 'regions.json')
   writeFileSync(file, JSON.stringify(regions))
 }
