2f17d4046091975241d0821c5b61767e06e49568
Ryan Block <rblock@gmail.com>
2023-10-17 10:18:29 -0700

    Enable plugin version script to bump all plugins

--- scripts/version-plugins.mjs
+++ scripts/version-plugins.mjs
@@ -1,8 +1,7 @@
 import { existsSync, readFileSync, writeFileSync } from 'node:fs'
 import { join } from 'node:path'
 import { execSync } from 'node:child_process'
-import { services } from '../src/services.js'
-import plugins from '../plugins.mjs'
+import pluginList from '../plugins.mjs'
 import semver from 'semver'
 const cwd = process.cwd()
 
@@ -13,59 +12,65 @@ if (!semverArgs.includes(action)) {
   throw ReferenceError(`Version action must be one of: ${semverArgs.join(', ')} (got: ${action})`)
 }
 
-const plugin = args[1].replace('-types', '')
-const typesOnly = args[1].endsWith('-types')
+const pluginName = args[1]
+const plugins = pluginName === 'all' ? pluginList.map(({ service }) => service) : [ pluginName ]
 
-if (!services.includes(plugin)) {
-  throw ReferenceError(`Invalid plugin: ${plugin}`)
-}
-if (!plugins.some(({ name }) => name === plugin)) {
-  throw ReferenceError(`Plugin does not yet exist: ${plugin}`)
-}
-if (typesOnly && action !== 'patch') {
-  throw ReferenceError(`Types packages may only receive patches independent of the main plugin`)
-}
+for (let p of plugins) {
+  const plugin = p.replace('-types', '')
+  const typesOnly = p.endsWith('-types')
 
-const pluginDir = join(cwd, 'plugins', plugin)
-const pluginPkgFile = join(pluginDir, 'package.json')
-const pluginTypeDir = join(pluginDir, 'types')
-const pluginTypePkgFile = join(pluginTypeDir, 'package.json')
-if (!existsSync(pluginDir) || !existsSync(pluginTypeDir)) {
-  throw ReferenceError(`Plugin or plugin types directory not found: ${plugin}`)
-}
-if (!existsSync(pluginPkgFile) || !existsSync(pluginTypePkgFile)) {
-  throw ReferenceError(`Plugin or types package.json file not found: ${plugin}`)
-}
+  const pluginDir = join(cwd, 'plugins', plugin)
 
-const status = execSync('git status --porcelain')
-if (status.length) {
-  console.error('Found uncommitted changes:')
-  console.error(status.toString())
-  console.error('Please stash or commit changes and run again')
-  process.exit(1)
-}
-
-const changes = {}
-if (!typesOnly) changes[pluginPkgFile] = { msg: `\`@aws-lite/${plugin}\` ` }
-changes[pluginTypePkgFile] = { msg: `\`@aws-lite/${plugin}-types\` ` }
-
-Object.keys(changes).forEach(file => {
-  const pkg = JSON.parse(readFileSync(file))
-  const newVersion = semver.inc(pkg.version, action)
-  if (typesOnly) {
-    changes[file].ver = newVersion
+  if (!existsSync(pluginDir)) {
+    throw ReferenceError(`Invalid plugin, or plugin doesn't yet exist: ${plugin}`)
   }
-  else if (file === pluginTypePkgFile &&
-           semver.lt(newVersion, changes[pluginPkgFile].ver)) {
-    changes[file].ver = changes[pluginPkgFile].ver
+  if (!pluginList.some(({ service }) => service === plugin)) {
+    throw ReferenceError(`Plugin not found in aws-lite plugins list: ${plugin}`)
   }
-  else changes[file].ver = newVersion
+  if (typesOnly && action !== 'patch') {
+    throw ReferenceError(`Types packages may only receive patches independent of the main plugin`)
+  }
 
-  pkg.version = changes[file].ver
-  changes[file].msg = changes[file].msg += changes[file].ver
-  writeFileSync(file, JSON.stringify(pkg, null, 2) + '\n')
-})
+  const pluginPkgFile = join(pluginDir, 'package.json')
+  const pluginTypeDir = join(pluginDir, 'types')
+  const pluginTypePkgFile = join(pluginTypeDir, 'package.json')
+  if (!existsSync(pluginDir) || !existsSync(pluginTypeDir)) {
+    throw ReferenceError(`Plugin or plugin types directory not found: ${plugin}`)
+  }
+  if (!existsSync(pluginPkgFile) || !existsSync(pluginTypePkgFile)) {
+    throw ReferenceError(`Plugin or types package.json file not found: ${plugin}`)
+  }
 
-const files = Object.keys(changes).join(' ')
-const msg = Object.values(changes).map(({ msg }) => msg).join('\n')
-execSync(`git commit ${files} -m '${msg}'`)
+  const status = execSync('git status --porcelain')
+  if (status.length) {
+    console.error('Found uncommitted changes:')
+    console.error(status.toString())
+    console.error('Please stash or commit changes and run again')
+    process.exit(1)
+  }
+
+  const changes = {}
+  if (!typesOnly) changes[pluginPkgFile] = { msg: `\`@aws-lite/${plugin}\` ` }
+  changes[pluginTypePkgFile] = { msg: `\`@aws-lite/${plugin}-types\` ` }
+
+  Object.keys(changes).forEach(file => {
+    const pkg = JSON.parse(readFileSync(file))
+    const newVersion = semver.inc(pkg.version, action)
+    if (typesOnly) {
+      changes[file].ver = newVersion
+    }
+    else if (file === pluginTypePkgFile &&
+             semver.lt(newVersion, changes[pluginPkgFile].ver)) {
+      changes[file].ver = changes[pluginPkgFile].ver
+    }
+    else changes[file].ver = newVersion
+
+    pkg.version = changes[file].ver
+    changes[file].msg = changes[file].msg += changes[file].ver
+    writeFileSync(file, JSON.stringify(pkg, null, 2) + '\n')
+  })
+
+  const files = Object.keys(changes).join(' ')
+  const msg = Object.values(changes).map(({ msg }) => msg).join('\n')
+  execSync(`git commit ${files} -m '${msg}'`)
+}
