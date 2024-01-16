d5229c5ba98731ad082e5f2064aa4abf35421a23
Pat Patterson <pat@backblaze.com>
2024-01-16 14:26:44 -0800

    Bind s3rver to 0.0.0.0

--- test/plugins/s3/s3-test.js
+++ test/plugins/s3/s3-test.js
@@ -41,7 +41,7 @@ test('Set up env', async t => {
 
   s3rver = new S3rver({
     port,
-    address: '127.0.0.1',
+    address: '0.0.0.0',
     silent: true,
     serviceEndpoint: serviceEndpoint,
     directory: join(tmp, s3_root_dir),
