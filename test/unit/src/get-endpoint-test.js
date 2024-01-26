8ccc3c94b2b24785fcb8f836795eed4e84b72625
Ryan Block <rblock@gmail.com>
2024-01-25 17:42:03 -0800

    Update endpoint getter tests

--- test/unit/src/get-endpoint-test.js
+++ test/unit/src/get-endpoint-test.js
@@ -5,7 +5,7 @@ let mockTmp = require('mock-tmp')
 let { defaults, overrideHomedir, resetAWSEnvVars } = require('../../lib')
 let cwd = process.cwd()
 let sut = join(cwd, 'src', 'get-endpoint.js')
-let getHost = require(sut)
+let getEndpoint = require(sut)
 
 let { profile } = defaults
 let mock = join(cwd, 'test', 'mock')
@@ -17,7 +17,7 @@ let host = 'aws-lite.org'
 
 test('Set up env', t => {
   t.plan(1)
-  t.ok(getHost, 'getHost module is present')
+  t.ok(getEndpoint, 'getEndpoint module is present')
 })
 
 test('Get host from passed params', async t => {
@@ -25,16 +25,16 @@ test('Get host from passed params', async t => {
   resetAWSEnvVars()
   let result
 
-  result = await getHost({ host: fullHost })
+  result = await getEndpoint({ host: fullHost })
   t.equal(result, host, 'Returned correct host from passed params')
 
   // Prioritize passed params before env or config file
   process.env.AWS_ENDPOINT_URL = localhost
-  result = await getHost({ host: fullHost })
+  result = await getEndpoint({ host: fullHost })
   t.equal(result, host, 'Returned correct host from passed params')
   resetAWSEnvVars()
 
-  result = await getHost({ awsConfigFile: configMock, host: fullHost })
+  result = await getEndpoint({ awsConfigFile: configMock, host: fullHost })
   t.equal(result, host, 'Returned correct host from passed params')
 })
 
@@ -44,13 +44,13 @@ test('Get host from env vars', async t => {
   let result
 
   process.env.AWS_ENDPOINT_URL = localhost
-  result = await getHost({})
+  result = await getEndpoint({})
   t.equal(result, localhost, 'Returned correct host from env var')
   resetAWSEnvVars()
 
   // Prioritize env vars before config file
   process.env.AWS_ENDPOINT_URL = localhost
-  result = await getHost({ awsConfigFile: configMock })
+  result = await getEndpoint({ awsConfigFile: configMock })
   t.equal(result, localhost, 'Returned correct host from env var')
   resetAWSEnvVars()
 })
@@ -68,25 +68,25 @@ test('Get host from config file', async t => {
   let homedir = mockTmp({ [configFile]: readFileSync(configMock) })
   overrideHomedir(homedir)
   process.env.AWS_SDK_LOAD_CONFIG = true
-  result = await getHost({ profile })
+  result = await getEndpoint({ profile })
   t.equal(result, awsUrl, 'Returned correct host from config file (~/.aws file location) via env var')
   resetAWSEnvVars()
 
-  result = await getHost({ awsConfigFile: true, profile })
+  result = await getEndpoint({ awsConfigFile: true, profile })
   t.equal(result, awsUrl, 'Returned correct host from config file (~/.aws file location) via param')
   mockTmp.reset()
 
   // Configured file locations
   process.env.AWS_SDK_LOAD_CONFIG = true
   process.env.AWS_CONFIG_FILE = configMock
-  result = await getHost({ profile })
+  result = await getEndpoint({ profile })
   t.equal(result, awsUrl, 'Returned correct host from config file (default profile) via env var')
   resetAWSEnvVars()
 
-  result = await getHost({ awsConfigFile: configMock, profile })
+  result = await getEndpoint({ awsConfigFile: configMock, profile })
   t.equal(result, awsUrl, 'Returned correct host from config file (default profile) via param')
 
-  result = await getHost({ awsConfigFile: configMock, profile: profile1 })
+  result = await getEndpoint({ awsConfigFile: configMock, profile: profile1 })
   t.equal(result, lolUrl, 'Returned correct host from config file (!default profile) via param')
 })
 
@@ -94,10 +94,10 @@ test('Host not found (and that is ok)', async t => {
   t.plan(2)
   let result
 
-  result = await getHost({ awsConfigFile: configMock, profile })
+  result = await getEndpoint({ awsConfigFile: configMock, profile })
   t.ok(result, 'Got a host from a profile that has a host')
 
-  result = await getHost({ awsConfigFile: configMock, profile: 'profile_2' })
+  result = await getEndpoint({ awsConfigFile: configMock, profile: 'profile_2' })
   t.equal(result, undefined, 'Did not get a host from a profile that has no host')
 })
 
@@ -105,19 +105,19 @@ test('Parse URLs', async t => {
   t.plan(5)
   let result
 
-  result = await getHost({ host: 'http://localhost' })
+  result = await getEndpoint({ host: 'http://localhost' })
   t.equal(result, localhost, 'Returned correctly parsed host')
 
-  result = await getHost({ host: 'http://localhost:3333' })
+  result = await getEndpoint({ host: 'http://localhost:3333' })
   t.equal(result, localhost, 'Returned correctly parsed host')
 
-  result = await getHost({ host: localhost })
+  result = await getEndpoint({ host: localhost })
   t.equal(result, localhost, 'Returned correctly parsed host')
 
-  result = await getHost({ host: fullHost })
+  result = await getEndpoint({ host: fullHost })
   t.equal(result, host, 'Returned correctly parsed host')
 
-  result = await getHost({ host: host })
+  result = await getEndpoint({ host: host })
   t.equal(result, host, 'Returned correctly parsed host')
 })
 
