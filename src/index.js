eb513c94abebc3d70f641c70cb5025ec24e43730
Ryan Block <rblock@gmail.com>
2024-02-06 11:00:55 -0800

    Update types

--- src/index.js
+++ src/index.js
@@ -12,13 +12,13 @@ let clientFactory = require('./client-factory')
  * @param {string} [config.region] AWS service region (e.g. `us-west-1`); if not provided, defaults to `AWS_REGION`, `AWS_DEFAULT_REGION`, or `AMAZON_REGION` env vars
  * @param {string} [config.profile='default'] AWS config + credentials profile; if not provided, defaults to `AWS_PROFILE` env var, and then to the `default` profile, if present
  * @param {boolean} [config.verifyService=true] Validate service name against aws-lite's known services
- * @param {boolean} [config.autoloadPlugins=true] Automatically load installed `@aws-lite/*` + `aws-lite-plugin-*` plugins
+ * @param {boolean} [config.autoloadPlugins=false] Automatically load installed `@aws-lite/*` + `aws-lite-plugin-*` plugins; not suggested for production use
  * @param {boolean|string} [config.awsConfigFile=false] Load configuration via ~/.aws/config (boolean), or via a passed file path
  * @param {boolean} [config.debug] Enable debug logging to console
  * @param {string} [config.pathPrefix] Add a path prefix to requests, helpful for local testing
  * @param {string} [config.host] Set a custom host name to use, helpful for local testing
  * @param {boolean} [config.keepAlive=true] Disable Node.js's connection keep-alive, helpful for local testing
- * @param {array} [config.plugins] Manually define plugins to load; by default, `@aws-lite/*` + `aws-lite-plugin-*` plugins are auto-loaded
+ * @param {array} [config.plugins] Specify service plugins; each plugin can be a plugin object or a require or import statement
  * @param {number} [config.port] Set a custom port number to use, helpful for local testing
  * @param {string} [config.protocol='https'] Set the connection protocol to 'http', helpful for local testing
  * @param {string} [config.responseContentType] Set an overriding Content-Type headers for responses, helpful for local testing
