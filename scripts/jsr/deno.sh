b0166926b367dbab576e5ab2b812a2d345035ce5
Ryan Block <rblock@gmail.com>
2024-03-04 10:26:05 -0800

    Deno / JSR WIP

---
+++ scripts/jsr/deno.sh
@@ -0,0 +1,3 @@
+test/scripts/copy-src-tree.mjs
+
+# deno run --allow-read --allow-write --allow-env --allow-net --node-modules-dir test/unit/src/whatever.mjs | tap-arc
---
+++ scripts/jsr/readme.md
@@ -0,0 +1,2 @@
+Deno + JSR support is still experimental
+See: https://github.com/architect/aws-lite/issues/109
