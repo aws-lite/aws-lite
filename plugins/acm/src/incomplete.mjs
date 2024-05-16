9c8a5b813750df5d15adc84fabdd0edf6e8ed302
Adam Spilchen <16adam61@gmail.com>
2024-05-16 12:19:30 -0700

    Add `@aws-lite/acm` `DeleteCertificate`, `DescribeCertificate`, `ListCertificates`, `RequestCertificate` methods

--- plugins/acm/src/incomplete.mjs
+++ plugins/acm/src/incomplete.mjs
@@ -2,18 +2,14 @@ const disabled = true
 const docRoot = 'https://docs.aws.amazon.com/acm/latest/APIReference/'
 export default {
   AddTagsToCertificate:      { disabled, awsDoc: docRoot + 'API_AddTagsToCertificate.html' },
-  //   DeleteCertificate:         { disabled, awsDoc: docRoot + 'API_DeleteCertificate.html' },
-  //   DescribeCertificate:       { disabled, awsDoc: docRoot + 'API_DescribeCertificate.html' },
   ExportCertificate:         { disabled, awsDoc: docRoot + 'API_ExportCertificate.html' },
   GetAccountConfiguration:   { disabled, awsDoc: docRoot + 'API_GetAccountConfiguration.html' },
   GetCertificate:            { disabled, awsDoc: docRoot + 'API_GetCertificate.html' },
   ImportCertificate:         { disabled, awsDoc: docRoot + 'API_ImportCertificate.html' },
-  //   ListCertificates:          { disabled, awsDoc: docRoot + 'API_ListCertificates.html' },
   ListTagsForCertificate:    { disabled, awsDoc: docRoot + 'API_ListTagsForCertificate.html' },
   PutAccountConfiguration:   { disabled, awsDoc: docRoot + 'API_PutAccountConfiguration.html' },
   RemoveTagsFromCertificate: { disabled, awsDoc: docRoot + 'API_RemoveTagsFromCertificate.html' },
   RenewCertificate:          { disabled, awsDoc: docRoot + 'API_RenewCertificate.html' },
-  //   RequestCertificate:        { disabled, awsDoc: docRoot + 'API_RequestCertificate.html' },
   ResendValidationEmail:     { disabled, awsDoc: docRoot + 'API_ResendValidationEmail.html' },
   UpdateCertificateOptions:  { disabled, awsDoc: docRoot + 'API_UpdateCertificateOptions.html' },
 }
